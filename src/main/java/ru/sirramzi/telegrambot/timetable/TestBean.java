0a80a605501bd3a1455265d04203e2400414ab45
SirRamzi <aleks.aleksp@gmail.com>
2023-01-27 19:56:09 +0400

    Add TestBean class for create new bean

---
+++ src/main/java/ru/sirramzi/telegrambot/timetable/TestBean.java
@@ -0,0 +1,19 @@
+package ru.sirramzi.telegrambot.timetable;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
