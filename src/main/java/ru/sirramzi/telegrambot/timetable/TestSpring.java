0c0d7100d33dbb1a1fba276e73aa3ee719780324
SirRamzi <aleks.aleksp@gmail.com>
2023-02-02 22:58:31 +0400

    Add IoC

--- src/main/java/ru/sirramzi/telegrambot/timetable/TestSpring.java
+++ src/main/java/ru/sirramzi/telegrambot/timetable/TestSpring.java
@@ -6,7 +6,8 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Music musicBean = context.getBean("musicBean", Music.class);
-        System.out.println(musicBean.getSong());
+        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
+        musicPlayer.playMusic();
         context.close();
     }
 }
